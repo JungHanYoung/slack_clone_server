976c81f41d99cdc14d241769d814babc65d65408
Hanyoung Jeong <8735132@gmail.com>
2018-05-07 09:25:14 +0900

    typeorm 동작 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,41 @@
 import { GraphQLServer } from "graphql-yoga";
+import { User } from "./entity/User";
+import { createConnection } from "typeorm";
 
 const typeDefs = `
     type Query {
         hello: String
+    }
+
+    type Mutation {
+        register(email: String!, password: String!): Boolean!
     }
 `;
 
 const resolvers = {
     Query: {
         hello: () => 'hi'
+    },
+    Mutation: {
+        register: async (_: any, args: any) => {
+            try {
+                await User.create({
+                    email: args.email,
+                    password: args.password
+                }).save();
+                return true;
+            } catch(err) {
+                console.log(err);
+                return false;
+            }
+        }
     }
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
-server.start({ port: 4000 }).then(() => {
-    console.log("server listening on port", 4000);
+createConnection().then(() => {
+    server.start({ port: 4000 }).then(() => {
+        console.log("server listening on port", 4000);
+    });
 })
